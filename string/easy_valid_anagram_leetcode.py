7b55b65a06ce6eb121628884037e77144438bd71
Soham Gupta <115832149+sohamgupta100@users.noreply.github.com>
2023-09-23 17:36:20 +0530

    Create easy_valid_anagram_leetcode.py

---
+++ string/easy_valid_anagram_leetcode.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        s_sorted = ''.join(sorted(s))
+        t_sorted = ''.join(sorted(t))
+
+        if s_sorted == t_sorted:
+            return True
+        
+        return False
