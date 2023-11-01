2ec931b9af77c0dbd5fd6d450b701a889e6e8a29
Soham Gupta <115832149+sohamgupta100@users.noreply.github.com>
2023-11-02 00:56:01 +0530

    Create easy_symmetric_tree.cpp

---
+++ binary-trees/easy_symmetric_tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        
+        if (!left && !right)
+        {
+            return true;
+        }
+        
+        if (!left || !right)
+        {
+            return false;
+        }
+        
+        bool x = isMirror(left->left, right->right);
+        bool y = isMirror(left->right, right->left);
+        return (left->val == right->val) && x && y;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root)
+        {
+            return true;
+        }
+
+        return isMirror(root->left, root->right);
+    }
+};
