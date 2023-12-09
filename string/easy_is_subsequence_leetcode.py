474c66926f5aa65548a75e8a3c3ab33aa95859ea
Soham Gupta <115832149+sohamgupta100@users.noreply.github.com>
2023-12-10 00:50:15 +0530

    Create easy_is_subsequence_leetcode.py

---
+++ string/easy_is_subsequence_leetcode.py
@@ -0,0 +1,36 @@
+'''
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) > len(t):
+            return False
+        
+        if len(s) == 0:
+            return True
+
+        subseq = 0
+
+        for i in range(0, len(t)):
+            if subseq < len(s):
+                if s[subseq] == t[i]:
+                    subseq += 1
+
+        return subseq == len(s)
