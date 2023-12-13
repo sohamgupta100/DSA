91eeb6f746226559137bc8d2214eeff1ff883f7e
Soham Gupta <115832149+sohamgupta100@users.noreply.github.com>
2023-12-14 01:26:39 +0530

    Create easy_find_words_that_can_be_formed_by_characters_leetcode.java

---
+++ string/easy_find_words_that_can_be_formed_by_characters_leetcode.java
@@ -0,0 +1,60 @@
+/*
+You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once).
+
+Return the sum of lengths of all good strings in words.
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+Example 2:
+
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+*/
+
+class Solution {
+    public int countCharacters(String[] words, String chars) 
+    {
+        int[] counts = new int[26];
+
+        for(int i=0; i < chars.length(); i++)
+        {
+            counts[chars.charAt(i) - 'a']++;
+        }
+
+        int result = 0;
+
+        for(String s : words)
+        {
+            if(isMatch(s, counts))
+            {
+                result += s.length();
+            }
+        }
+
+        return result;
+    }
+
+    boolean isMatch(String s, int[] counts)
+    {
+        int[] counts2 = new int[26];
+
+        for(int i=0; i<s.length(); i++)
+        {
+            int diff = s.charAt(i) - 'a';
+            counts2[diff]++;
+
+            if(counts2[diff] > counts[diff])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
